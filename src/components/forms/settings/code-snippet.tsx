e2f700e4acd09885c2fd81c0db560158d9282ccf
Sumit3972 <124435686+Sumit3972@users.noreply.github.com>
2024-09-05 01:06:02 +0530

    Update code-snippet.tsx

--- src/components/forms/settings/code-snippet.tsx
+++ src/components/forms/settings/code-snippet.tsx
@@ -14,33 +14,32 @@ const CodeSnippet = ({ id }: Props) => {
     const iframe = document.createElement("iframe");
     
     const iframeStyles = (styleString) => {
-      const style = document.createElement('style');
-      style.textContent = styleString;
-      document.head.append(style);
+    const style = document.createElement('style');
+    style.textContent = styleString;
+    document.head.append(style);
     }
     
-    iframeStyles(\`
-      .chat-frame {
-        position: fixed;
-        bottom: 50px;
-        right: 50px;
-        border: none;
-        z-index: 1000;
-      }
-    \`)
+    iframeStyles('
+        .chat-frame {
+            position: fixed;
+            bottom: 50px;
+            right: 50px;
+            border: none;
+        }
+    ')
     
-    iframe.src = "https://corinna-9a1qpl4ij-sumit-mehtas-projects.vercel.app/chatbot";
-    iframe.classList.add('chat-frame');
-    document.body.appendChild(iframe);
+    iframe.src = "https://corinna-ai-nine.vercel.app/chatbot"
+    iframe.classList.add('chat-frame')
+    document.body.appendChild(iframe)
     
     window.addEventListener("message", (e) => {
-      if (e.origin !== "https://corinna-9a1qpl4ij-sumit-mehtas-projects.vercel.app") return;
-      let dimensions = JSON.parse(e.data);
-      iframe.width = dimensions.width;
-      iframe.height = dimensions.height;
-      iframe.contentWindow.postMessage("${id}", "https://corinna-9a1qpl4ij-sumit-mehtas-projects.vercel.app/");
+        if(e.origin !== "https://corinna-ai-nine.vercel.app/") return null
+        let dimensions = JSON.parse(e.data)
+        iframe.width = dimensions.width
+        iframe.height = dimensions.height
+        iframe.contentWindow.postMessage("${id}", "https://corinna-ai-nine.vercel.app/")
     })
-  `
+        `
 
   return (
     <div className="mt-10 flex flex-col gap-5 items-start">
