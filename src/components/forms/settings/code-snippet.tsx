a8aea4b11384738dc4a1fe32b2c4af464799dd19
Sumit3972 <124435686+Sumit3972@users.noreply.github.com>
2024-09-05 00:35:42 +0530

    Update code-snippet.tsx

--- src/components/forms/settings/code-snippet.tsx
+++ src/components/forms/settings/code-snippet.tsx
@@ -10,13 +10,13 @@ type Props = {
 
 const CodeSnippet = ({ id }: Props) => {
   const { toast } = useToast()
-const snippet = `
+  let snippet = `
     const iframe = document.createElement("iframe");
     
     const iframeStyles = (styleString) => {
-        const style = document.createElement('style');
-        style.textContent = styleString;
-        document.head.append(style);
+    const style = document.createElement('style');
+    style.textContent = styleString;
+    document.head.append(style);
     }
     
     iframeStyles('
@@ -28,18 +28,18 @@ const snippet = `
         }
     ')
     
-    iframe.src = "https://corinna-k0ir0xdet-sumit-mehtas-projects.vercel.app/"
+    iframe.src = "http://localhost:3000/chatbot"
     iframe.classList.add('chat-frame')
     document.body.appendChild(iframe)
     
     window.addEventListener("message", (e) => {
-        if(e.origin !== "https://corinna-k0ir0xdet-sumit-mehtas-projects.vercel.app") return null
+        if(e.origin !== "http://localhost:3000") return null
         let dimensions = JSON.parse(e.data)
         iframe.width = dimensions.width
         iframe.height = dimensions.height
-        iframe.contentWindow.postMessage("${id}", "https://corinna-k0ir0xdet-sumit-mehtas-projects.vercel.app/")
+        iframe.contentWindow.postMessage("${id}", "http://localhost:3000/")
     })
-`
+        `
 
   return (
     <div className="mt-10 flex flex-col gap-5 items-start">
