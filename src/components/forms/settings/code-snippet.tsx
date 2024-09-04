f5ed0af09b8d8855012882069cd9cc21c8d1e5ae
Sumit3972 <124435686+Sumit3972@users.noreply.github.com>
2024-09-05 00:58:23 +0530

    Update code-snippet.tsx

--- src/components/forms/settings/code-snippet.tsx
+++ src/components/forms/settings/code-snippet.tsx
@@ -1,29 +1,70 @@
-const iframe = document.createElement("iframe");
+'use client'
+import Section from '@/components/section-label'
+import { useToast } from '@/components/ui/use-toast'
+import { Copy } from 'lucide-react'
+import React from 'react'
 
-const iframeStyles = (styleString) => {
-  const style = document.createElement('style');
-  style.textContent = styleString;
-  document.head.append(style);
+type Props = {
+  id: string
 }
 
-iframeStyles(`
-  .chat-frame {
-    position: fixed;
-    bottom: 50px;
-    right: 50px;
-    border: none;
-    z-index: 1000;
-  }
-`);
+const CodeSnippet = ({ id }: Props) => {
+  const { toast } = useToast()
+  let snippet = `
+    const iframe = document.createElement("iframe");
+    
+    const iframeStyles = (styleString) => {
+      const style = document.createElement('style');
+      style.textContent = styleString;
+      document.head.append(style);
+    }
+    
+    iframeStyles(\`
+      .chat-frame {
+        position: fixed;
+        bottom: 50px;
+        right: 50px;
+        border: none;
+        z-index: 1000;
+      }
+    \`)
+    
+    iframe.src = "https://corinna-9a1qpl4ij-sumit-mehtas-projects.vercel.app/chatbot";
+    iframe.classList.add('chat-frame');
+    document.body.appendChild(iframe);
+    
+    window.addEventListener("message", (e) => {
+      if (e.origin !== "https://corinna-9a1qpl4ij-sumit-mehtas-projects.vercel.app") return;
+      let dimensions = JSON.parse(e.data);
+      iframe.width = dimensions.width;
+      iframe.height = dimensions.height;
+      iframe.contentWindow.postMessage("${id}", "https://corinna-9a1qpl4ij-sumit-mehtas-projects.vercel.app/");
+    })
+  `
 
-iframe.src = "https://corinna-9a1qpl4ij-sumit-mehtas-projects.vercel.app/chatbot";
-iframe.classList.add('chat-frame');
-document.body.appendChild(iframe);
+  return (
+    <div className="mt-10 flex flex-col gap-5 items-start">
+      <Section
+        label="Code snippet"
+        message="Copy and paste this code snippet into the header tag of your website"
+      />
+      <div className="bg-cream px-10 rounded-lg inline-block relative">
+        <Copy
+          className="absolute top-5 right-5 text-gray-400 cursor-pointer"
+          onClick={() => {
+            navigator.clipboard.writeText(snippet)
+            toast({
+              title: 'Copied to clipboard',
+              description: 'You can now paste the code inside your website',
+            })
+          }}
+        />
+        <pre>
+          <code className="text-gray-500">{snippet}</code>
+        </pre>
+      </div>
+    </div>
+  )
+}
 
-window.addEventListener("message", (e) => {
-  if (e.origin !== "https://corinna-9a1qpl4ij-sumit-mehtas-projects.vercel.app") return;
-  let dimensions = JSON.parse(e.data);
-  iframe.width = dimensions.width;
-  iframe.height = dimensions.height;
-  iframe.contentWindow.postMessage("${id}", "https://corinna-9a1qpl4ij-sumit-mehtas-projects.vercel.app/");
-});
+export default CodeSnippet
